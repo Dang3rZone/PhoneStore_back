117b0a7de875fc1288131f69f6eee778dc4d6d73
Andres <andresrosalespinzon@gmail.com>
2021-04-14 21:31:04 +0200

    then catch

--- dbConfig.js
+++ dbConfig.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const mongoUrl = process.env.HOST;
 
-mongoose.connect(mongoUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Database Connected Successfully'))
+  .catch((err) => console.log(err));
