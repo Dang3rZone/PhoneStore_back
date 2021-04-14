42d3d731e81d6d6be13b204430d4ca6a5581280c
Andres <andresrosalespinzon@gmail.com>
2021-04-14 21:48:41 +0200

    changing name of variables

--- dbConfig.js
+++ dbConfig.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const mongoUrl = process.env.HOST;
+const uri = process.env.MONGODB_URI;
 
 mongoose
-  .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Database Connected Successfully'))
   .catch((err) => console.log(err));
